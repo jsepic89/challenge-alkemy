4e67c907b2696baf1bf3e2fd235cd29d41e0da27
Javier Sepic <javier.sepic@gmail.com>
2022-07-03 16:07:31 -0300

    added jsonwebtoken to login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 //note that as the User model already has the method for hashing the password, the password received via 
 // body will be hashed before being stored 
@@ -25,7 +26,8 @@ const userRegister = async (req, res) => {
     
 };
 
-
+// userLogin will check if the email exists in the database, and if the password is correct via bcrypt.compare
+// if credentials are OK, a jsonwebtoken for 30 days is generated. Name, id and token are returned via json
 const userLogin = async (req, res) => {
     const { email, password } = await req.body;
     const isUser = await User.findOne({ where: {email}});
@@ -39,8 +41,18 @@ const userLogin = async (req, res) => {
     if (!correctPassword){
         return res.json("Invalid password");
     }
-    console.log("Login successful");
-    return res.json("Login successful");
+
+    const token = jwt.sign({
+        name: isUser.name,
+        id: isUser.id
+    }, process.env.JWT_KEY, { expiresIn: "30d" });
+
+    return res.json({
+        message: "Login successful",
+        name: isUser.name,
+        id: isUser.id,
+        token
+    });
 };
 
 export { userRegister, userLogin };
